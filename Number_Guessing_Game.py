58d7870987b318e078fa0572fb471d6805ba2304
Luis Alberto Garcia Corona <garcialuis080918@gmail.com>
2023-04-20 11:18:45 -0300

    Number Guessing Game

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -1,43 +1,31 @@
 from random import randint
 
+easy = 10
+hard = 5
+
 def dificulty():
     print('Welcome to the Number Guessing Game')
     print('I´m thinking of a number between 1 and 100')
     dificult = str(input('Choose a difficulty. Type "Easy" or "Hard": ')).lower().strip()
     return dificult
 
+def lifes_game(answer, number, live):
+    if answer > number:
+        print('Too High\nGuess again')
+        return live += 1
+    elif answer < number:
+        print('Too Low\nGuess again')
+        return live += 1
+    else:
+        print(f'You got it! The answer was {answer}')
+
 
 def number_guessing_game(dificulty):
-    number_correct = randint(1, 101)
-    easy = 10
-    life = 0
-    hard = 5
-    if dificulty == "easy":
-        print('You have 10 attempts remaining to guess the number.')
-        while life < easy:
-            answer = int(input('Make a guess: '))
-            life +=1
-         if answer == number_correct:
-                print(f'You Win!!! The correct number was {number_correct} ')
-                break
-            elif answer > number_correct:
-                print('Too High\nGuess again')
-            elif answer < number_correct:
-                print('Too Low\nGuess again')
-            print(f'You have {easy - life} attempts remaining to guess the number')
-    else:
-        print('You have 5 attempts remaining to guess the number.')
-        while life < hard:
-            answer = int(input('Make a guess: '))
-            life += 1
-            if answer == number_correct:
-                print(f'You Win!!! The correct number was {number_correct} ')
-                break
-            elif answer > number_correct:
-                print('Too High\nGuess again')
-            elif answer < number_correct:
-                print('Too Low\nGuess again')
-            print(f'You have {hard  - life} attempts remaining to guess the number')
+    number= randint(1, 101)
+
+
+
+
 
 
 number_guessing_game(dificulty())
